213795d3d28b2c5acb04d34479b7a21c91ec4754
Kian <75130117+Kian1010@users.noreply.github.com>
2022-05-11 21:53:14 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace PokemonTypeCombos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.richtxtbx = new System.Windows.Forms.RichTextBox();
+            this.cmbx1 = new System.Windows.Forms.ComboBox();
+            this.cmbx2 = new System.Windows.Forms.ComboBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCalculate.Location = new System.Drawing.Point(332, 216);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(179, 40);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // richtxtbx
+            // 
+            this.richtxtbx.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.richtxtbx.Location = new System.Drawing.Point(35, 273);
+            this.richtxtbx.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.richtxtbx.Name = "richtxtbx";
+            this.richtxtbx.Size = new System.Drawing.Size(741, 154);
+            this.richtxtbx.TabIndex = 1;
+            this.richtxtbx.Text = "";
+            // 
+            // cmbx1
+            // 
+            this.cmbx1.BackColor = System.Drawing.SystemColors.Window;
+            this.cmbx1.DropDownHeight = 150;
+            this.cmbx1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cmbx1.FormattingEnabled = true;
+            this.cmbx1.IntegralHeight = false;
+            this.cmbx1.ItemHeight = 30;
+            this.cmbx1.Location = new System.Drawing.Point(286, 52);
+            this.cmbx1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.cmbx1.Name = "cmbx1";
+            this.cmbx1.Size = new System.Drawing.Size(254, 38);
+            this.cmbx1.TabIndex = 2;
+            this.cmbx1.Text = "Select Primary Type:";
+            // 
+            // cmbx2
+            // 
+            this.cmbx2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.cmbx2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cmbx2.DropDownHeight = 150;
+            this.cmbx2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cmbx2.FormattingEnabled = true;
+            this.cmbx2.IntegralHeight = false;
+            this.cmbx2.Location = new System.Drawing.Point(286, 148);
+            this.cmbx2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.cmbx2.Name = "cmbx2";
+            this.cmbx2.Size = new System.Drawing.Size(254, 38);
+            this.cmbx2.TabIndex = 3;
+            this.cmbx2.Text = "Select Secondary Type:";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl1.Location = new System.Drawing.Point(321, 9);
+            this.lbl1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(201, 30);
+            this.lbl1.TabIndex = 4;
+            this.lbl1.Text = "Enter Primary Type:";
+            this.lbl1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl2.Location = new System.Drawing.Point(296, 106);
+            this.lbl2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(235, 30);
+            this.lbl2.TabIndex = 5;
+            this.lbl2.Text = "Enter Secondary Type: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(828, 453);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.cmbx2);
+            this.Controls.Add(this.cmbx1);
+            this.Controls.Add(this.richtxtbx);
+            this.Controls.Add(this.btnCalculate);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Pokemon Type Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnCalculate;
+        private RichTextBox richtxtbx;
+        private ComboBox cmbx1;
+        private ComboBox cmbx2;
+        private Label lbl1;
+        private Label lbl2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,307 @@
+namespace PokemonTypeCombos
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public enum PokemonTypes
+        {
+            Normal, Fighting, Flying, Poison,
+            Ground, Rock, Bug, Ghost,
+            Steel, Fire, Water, Grass,
+            Electric, Psychic, Ice, Dragon,
+            Dark, Fairy
+        }
+        public float[] ArrOutput1 = new float[18];
+        public float[] ArrOutput2 = new float[18];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbx2.Items.Add("None");
+
+            foreach (var item in Enum.GetNames<PokemonTypes>())
+            {
+                cmbx1.Items.Add(item);
+                cmbx2.Items.Add(item);
+             
+            }
+       
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < ArrOutput1.Length; i++)
+            {
+                ArrOutput1[i] = 1;
+                ArrOutput2[i] = 1;
+            }
+            if (CatchErrors())
+            {
+                return;
+            } 
+            FindStrengthsAndWeaknesses(ArrOutput1, cmbx1.Text);
+            FindStrengthsAndWeaknesses(ArrOutput2 , cmbx2.Text);
+            DisplayInfo(); 
+        }
+
+        private void DisplayInfo()
+        {
+            string immune = "This typing is immune to: ";
+            string resistant = "This typing is resistant against: ";
+            string weak = "This typing is weak against: ";
+            string neutral = "This typing is damaged normally by: ";
+            int immunecount = 0;
+            int resistantcount = 0;
+
+            for (int i = 0; i < ArrOutput1.Length; i++)
+            {
+                ArrOutput1[i] = ArrOutput1[i] * ArrOutput2[i];
+
+                switch (ArrOutput1[i])
+                {
+                    case 4:
+                        weak += $" {(PokemonTypes)i}(Quad weakness), " ;
+                        break;
+                    case 2:
+                        weak += $" {(PokemonTypes)i}, ";
+                        break;
+                    case 0.5f:
+                        resistant += $" {(PokemonTypes)i}, ";
+                        resistantcount++;
+                        break;
+                    case 0.25f:
+                        resistant += $" {(PokemonTypes)i}, ";
+                        resistantcount++;
+                        break;
+                    case 0f:
+                        immune += $" {(PokemonTypes)i}, ";
+                        immunecount++;
+                        break;
+                    case 1f:
+                        neutral += $" {(PokemonTypes)i}, ";
+                        break;
+                    default:
+
+                        break;
+                }
+            }//for 
+
+            if (immunecount == 0)
+            {
+                immune += " None";
+            }
+            if (resistantcount == 0)
+            {
+                resistant += " None";
+            }
+
+            richtxtbx.Text = $"{immune}\n{resistant}\n{weak}\n{neutral}";
+        }
+
+
+
+        private Boolean CatchErrors()
+        {         
+            if (cmbx1.SelectedIndex <= -1) 
+            {
+                MessageBox.Show("incorrect info in box1");
+                return true;
+            }
+
+            if (cmbx2.SelectedIndex <= -1)
+            {
+                MessageBox.Show("incorrect info in box2");
+                return true;
+            }
+
+            if (cmbx1.Text == cmbx2.Text)
+            {
+                MessageBox.Show("Cannot have same type twice");
+                return true;
+            }
+
+            return false;
+        }
+
+        private void FindStrengthsAndWeaknesses(float[] ArrData, string textboxdata)
+        {
+
+            foreach (var item in Enum.GetValues<PokemonTypes>())
+            {
+                if (item.ToString() == textboxdata)
+                {
+                    switch (item)
+                    {
+                        case PokemonTypes.Normal:
+                            ArrData[(int)PokemonTypes.Fighting] = 2f;
+                            ArrData[(int)PokemonTypes.Ghost] = 0f;
+                            break;
+                        case PokemonTypes.Fighting:
+                            ArrData[(int)PokemonTypes.Psychic] = 2f;
+                            ArrData[(int)PokemonTypes.Flying] = 2f;
+                            ArrData[(int)PokemonTypes.Fairy] = 2f;
+                            ArrData[(int)PokemonTypes.Bug] = 0.5f;
+                            ArrData[(int)PokemonTypes.Rock] = 0.5f;
+                            ArrData[(int)PokemonTypes.Dark] = 0.5f;
+                            break;
+                        case PokemonTypes.Flying:
+                            ArrData[(int)PokemonTypes.Electric] = 2f;
+                            ArrData[(int)PokemonTypes.Ice] = 2f;
+                            ArrData[(int)PokemonTypes.Rock] = 2f;
+                            ArrData[(int)PokemonTypes.Grass] = 0.5f;
+                            ArrData[(int)PokemonTypes.Fighting] = 0.5f;
+                            ArrData[(int)PokemonTypes.Bug] = 0.5f;
+                            ArrData[(int)PokemonTypes.Ground] = 0f;
+                            break;
+                        case PokemonTypes.Poison:
+                            ArrData[(int)PokemonTypes.Ground] = 2f;
+                            ArrData[(int)PokemonTypes.Psychic] = 2f;
+                            ArrData[(int)PokemonTypes.Grass] = 0.5f;
+                            ArrData[(int)PokemonTypes.Fighting] = 0.5f;
+                            ArrData[(int)PokemonTypes.Poison] = 0.5f;
+                            ArrData[(int)PokemonTypes.Bug] = 0.5f;
+                            ArrData[(int)PokemonTypes.Fairy] = 0.5f;
+                            break;
+                        case PokemonTypes.Ground:
+                            ArrData[(int)PokemonTypes.Water] = 2f;
+                            ArrData[(int)PokemonTypes.Grass] = 2f;
+                            ArrData[(int)PokemonTypes.Ice] = 2f;
+                            ArrData[(int)PokemonTypes.Poison] = 0.5f;
+                            ArrData[(int)PokemonTypes.Rock] = 0.5f;
+                            ArrData[(int)PokemonTypes.Electric] = 0f;
+                            break;
+                        case PokemonTypes.Rock:
+                            ArrData[(int)PokemonTypes.Water] = 2f;
+                            ArrData[(int)PokemonTypes.Grass] = 2f;
+                            ArrData[(int)PokemonTypes.Fighting] = 2f;
+                            ArrData[(int)PokemonTypes.Ground] = 2f;
+                            ArrData[(int)PokemonTypes.Steel] = 2f;
+                            ArrData[(int)PokemonTypes.Normal] = 0.5f;
+                            ArrData[(int)PokemonTypes.Fire] = 0.5f;
+                            ArrData[(int)PokemonTypes.Poison] = 0.5f;
+                            ArrData[(int)PokemonTypes.Flying] = 0.5f;
+                            break;
+                        case PokemonTypes.Bug:
+                            ArrData[(int)PokemonTypes.Fire] = 2f;
+                            ArrData[(int)PokemonTypes.Flying] = 2f;
+                            ArrData[(int)PokemonTypes.Rock] = 2f;
+                            ArrData[(int)PokemonTypes.Grass] = 0.5f;
+                            ArrData[(int)PokemonTypes.Fighting] = 0.5f;
+                            ArrData[(int)PokemonTypes.Ground] = 0.5f;
+                            break;
+                        case PokemonTypes.Ghost:
+                            ArrData[(int)PokemonTypes.Dark] = 2f;
+                            ArrData[(int)PokemonTypes.Ghost] = 2f;
+                            ArrData[(int)PokemonTypes.Poison] = 0.5f;
+                            ArrData[(int)PokemonTypes.Bug] = 0.5f;
+                            ArrData[(int)PokemonTypes.Fighting] = 0f;
+                            ArrData[(int)PokemonTypes.Normal] = 0f;
+                            break;
+                        case PokemonTypes.Steel:
+                            ArrData[(int)PokemonTypes.Fire] = 2f;
+                            ArrData[(int)PokemonTypes.Fighting] = 2f;
+                            ArrData[(int)PokemonTypes.Ground] = 2f;
+                            ArrData[(int)PokemonTypes.Normal] = 0.5f;
+                            ArrData[(int)PokemonTypes.Grass] = 0.5f;
+                            ArrData[(int)PokemonTypes.Ice] = 0.5f;
+                            ArrData[(int)PokemonTypes.Flying] = 0.5f;
+                            ArrData[(int)PokemonTypes.Psychic] = 0.5f;
+                            ArrData[(int)PokemonTypes.Bug] = 0.5f;
+                            ArrData[(int)PokemonTypes.Rock] = 0.5f;
+                            ArrData[(int)PokemonTypes.Dragon] = 0.5f;
+                            ArrData[(int)PokemonTypes.Steel] = 0.5f;
+                            ArrData[(int)PokemonTypes.Fairy] = 0.5f;
+                            ArrData[(int)PokemonTypes.Poison] = 0f;
+                            break;
+                        case PokemonTypes.Fire:
+                            ArrData[(int)PokemonTypes.Water] = 2f;
+                            ArrData[(int)PokemonTypes.Ground] = 2f;
+                            ArrData[(int)PokemonTypes.Rock] = 2f;
+                            ArrData[(int)PokemonTypes.Bug] = 0.5f;
+                            ArrData[(int)PokemonTypes.Grass] = 0.5f;
+                            ArrData[(int)PokemonTypes.Fire] = 0.5f;
+                            ArrData[(int)PokemonTypes.Ice] = 0.5f;
+
+                            break;
+                        case PokemonTypes.Water:
+                            ArrData[(int)PokemonTypes.Electric] = 2f;
+                            ArrData[(int)PokemonTypes.Grass] = 2f;
+                            ArrData[(int)PokemonTypes.Fire] = 0.5f;
+                            ArrData[(int)PokemonTypes.Water] = 0.5f;
+                            ArrData[(int)PokemonTypes.Ice] = 0.5f;
+                            ArrData[(int)PokemonTypes.Steel] = 0.5f;
+                            break;
+                        case PokemonTypes.Grass:
+                            ArrData[(int)PokemonTypes.Fire] = 2f;
+                            ArrData[(int)PokemonTypes.Ice] = 2f;
+                            ArrData[(int)PokemonTypes.Poison] = 2f;
+                            ArrData[(int)PokemonTypes.Flying] = 2f;
+                            ArrData[(int)PokemonTypes.Bug] = 2f;
+                            ArrData[(int)PokemonTypes.Water] = 0.5f;
+                            ArrData[(int)PokemonTypes.Electric] = 0.5f;
+                            ArrData[(int)PokemonTypes.Grass] = 0.5f;
+                            ArrData[(int)PokemonTypes.Ground] = 0.5f;
+                            break;
+                        case PokemonTypes.Electric:
+                            ArrData[(int)PokemonTypes.Ground] = 2f;
+                            ArrData[(int)PokemonTypes.Flying] = 0.5f;
+                            ArrData[(int)PokemonTypes.Steel] = 0.5f;
+                            ArrData[(int)PokemonTypes.Electric] = 0.5f;
+                            break;
+                        case PokemonTypes.Psychic:
+                            ArrData[(int)PokemonTypes.Bug] = 2f;
+                            ArrData[(int)PokemonTypes.Ghost] = 2f;
+                            ArrData[(int)PokemonTypes.Dark] = 2f;
+                            ArrData[(int)PokemonTypes.Psychic] = 0.5f;
+                            ArrData[(int)PokemonTypes.Fighting] = 0.5f;
+                            break;
+                        case PokemonTypes.Ice:
+                            ArrData[(int)PokemonTypes.Steel] = 2f;
+                            ArrData[(int)PokemonTypes.Rock] = 2f;
+                            ArrData[(int)PokemonTypes.Fire] = 2f;
+                            ArrData[(int)PokemonTypes.Fighting] = 2f;
+                            ArrData[(int)PokemonTypes.Ice] = 0.5f;
+                            break;
+                        case PokemonTypes.Dragon:
+                            ArrData[(int)PokemonTypes.Dragon] = 2f;
+                            ArrData[(int)PokemonTypes.Fairy] = 2f;
+                            ArrData[(int)PokemonTypes.Ice] = 2f;
+                            ArrData[(int)PokemonTypes.Fire] = 0.5f;
+                            ArrData[(int)PokemonTypes.Water] = 0.5f;
+                            ArrData[(int)PokemonTypes.Grass] = 0.5f;
+                            ArrData[(int)PokemonTypes.Electric] = 0.5f;
+                            break;
+                        case PokemonTypes.Dark:
+                            ArrData[(int)PokemonTypes.Fighting] = 2f;
+                            ArrData[(int)PokemonTypes.Fairy] = 2f;
+                            ArrData[(int)PokemonTypes.Bug] = 2f;
+                            ArrData[(int)PokemonTypes.Ghost] = 0.5f;
+                            ArrData[(int)PokemonTypes.Dark] = 0.5f;
+                            ArrData[(int)PokemonTypes.Psychic] = 0f;
+                            break;
+                        case PokemonTypes.Fairy:
+                            ArrData[(int)PokemonTypes.Poison] = 2f;
+                            ArrData[(int)PokemonTypes.Steel] = 2f;
+                            ArrData[(int)PokemonTypes.Fighting] = 0.5f;
+                            ArrData[(int)PokemonTypes.Bug] = 0.5f;
+                            ArrData[(int)PokemonTypes.Dark] = 0.5f;
+                            ArrData[(int)PokemonTypes.Dragon] = 0f;
+                            break;
+                        default:
+                            MessageBox.Show("not working");
+                            break;
+                    }//end of case statement
+                }//end of if statement
+            }
+        }
+    }
+}
+
+     
